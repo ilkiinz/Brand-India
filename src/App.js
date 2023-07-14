badb07822b855d6633e56db94928dca20343723c
Ilkiinz <aqayev2205@gmail.com>
2023-07-14 22:36:37 +0400

    second commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './App.css';
-import {  Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import About from './About';
 import  Nav  from "./Nav";
+import Footer from "./Footer"
 function App() {
   return (
     <div className='container'>
+      <Nav />
       <Routes>
-        <Route path='/' element={<Nav />} />
+
         <Route path='/about' element={<About />} />
       </Routes>
+     
+     
+      <Footer />
     </div>
 
   );
---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+import logo from "./assets/logo.svg"
+function Footer() {
+    return(
+        <div>men ayag hissesiyem</div>
+    )
+}
+
+export default Footer;
