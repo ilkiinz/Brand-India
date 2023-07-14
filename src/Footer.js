4b1e817ba5e1906df856977d436bb9b7aad57679
Ilkiinz <aqayev2205@gmail.com>
2023-07-14 23:23:56 +0400

    second commit

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,34 @@
 import React from "react";
 import "./Footer.css";
 import logo from "./assets/logo.svg"
+import WebFont from 'webfontloader';
+
 function Footer() {
     return(
-        <div>men ayag hissesiyem</div>
-    )
-}
+        <div className="footer">
+
+            
+                <div className="footer-block1">
+                    <p className="first-word">Company</p>
+                    <p>About Us</p>
+                    <p>Why Choose us</p>
+                    <p>Brand</p>
+                </div>
+
+                <div className="footer-block2">
+                    <p className="first-word">Resource</p>
+                    <p>Privarcy Policy</p>
+                    <p>Terms and Condition</p>
+                    <p>Contact Us</p>
+                </div>
+        
+                <div className="footer-block3">
+                    <p className="about-brand">Brand India Limited 2022</p>
+                </div>
+
+
+        </div>
+    );
+};
 
 export default Footer;
