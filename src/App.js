6d7b25826259ec176404d7fdb3e9b13a11650636
Ilkiinz <aqayev2205@gmail.com>
2023-07-15 19:21:26 +0400

    second commit

--- src/App.js
+++ src/App.js
@@ -4,12 +4,44 @@ import { Route, Routes } from 'react-router-dom';
 import About from './About';
 import  Nav  from "./Nav";
 import Footer from "./Footer"
+import vector from "./assets/Vector.svg"
 function App() {
   return (
     <div className='container'>
       <Nav />
-      <Routes>
+  
+     <div className='flag-block'>
 
+      <div className='get-touch'>
+       <p>Get involved in of the world’s fastest growing major economies</p>
+       <button>Get Touch</button>
+      </div>
+
+      <div className='flag'>
+      <img src={vector} alt='vector' />
+      </div>
+
+     </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      <Routes>
         <Route path='/about' element={<About />} />
       </Routes>
      
--- src/Footer.js
+++ src/Footer.js
@@ -40,7 +40,7 @@ function Footer() {
 
             </div>
 
-    
+
         </div>
     );
 };
