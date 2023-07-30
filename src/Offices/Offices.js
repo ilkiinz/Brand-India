f0a6d820e66539fa41015fd3b6145a3c728bf352
Ilkiinz <aqayev2205@gmail.com>
2023-07-30 18:53:39 +0400

    change src

--- src/Offices/Offices.js
+++ src/Offices/Offices.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Offices.css";
 import Nav from "../Navbar/Nav";
 import Footer from "../Footer/Footer";
-
+import Leeds from "../assets/Leeds.svg";
 function Offices() {
  return(
      <div className="wrap">
@@ -12,6 +12,17 @@ function Offices() {
              <h3 className="text-black">office located?</h3>
              <p>Brand India Limited is a company registered in<br /> England and Wales</p>
          </div>
+            
+         <div className="leeds-cont">
+             <img src={Leeds} alt="leeds" />
+
+             <div className="leeds-text">
+                <h3 className="text-b">Our head office is located in the</h3>
+                <h3 className="text-o">Guiseley, Leeds, England </h3>
+                <p>Join our superstar team in automating businesses all over the world</p>
+             </div>
+         </div>
+
          <Footer />
      </div>
        
