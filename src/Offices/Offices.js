edc55f94a887da35373f6093cc0071cb9591e057
Ilkiinz <aqayev2205@gmail.com>
2023-07-31 18:00:37 +0400

    change src

--- src/Offices/Offices.js
+++ src/Offices/Offices.js
@@ -3,6 +3,7 @@ import "./Offices.css";
 import Nav from "../Navbar/Nav";
 import Footer from "../Footer/Footer";
 import Leeds from "../assets/Leeds.svg";
+import Plus from "../assets/Plus.svg";
 function Offices() {
  return(
      <div className="wrap">
@@ -22,6 +23,56 @@ function Offices() {
                 <p>Join our superstar team in automating businesses all over the world</p>
              </div>
          </div>
+
+         <div className="office-cont">
+             <div className="office-head">
+                 <h4>UK Regional Offices</h4>
+             </div>
+
+             <div className="office-btns">
+                 <div className="btns">
+                     <div className="first">
+                         <h4>London</h4>
+                         <button> <img src={Plus} alt="plus" /></button>
+                     </div>
+                     <div className="second">
+                         <h4>Cardiff</h4>
+                         <button> <img src={Plus} alt="plus" /></button>
+                     </div>
+                 </div>
+
+                 <div className="btns1">
+                     <div className="third">
+                         <h4>Edinburgh</h4>
+                         <button> <img src={Plus} alt="plus" /></button>
+                     </div>
+
+                     <div className="fourth">
+                         <h4>West Midlands</h4>
+                         <button> <img src={Plus} alt="plus" /></button>
+                     </div>
+                 </div>
+             </div>
+
+             <div>
+             <div className="office-head">
+                 <h4>India Office</h4>
+             </div>
+              
+             <div className="adress-cont">
+              <div className="adress">
+                <h4>519, Udyog Vihar Phase V</h4>
+              </div>
+                <div className="adress1">
+                    <h4>Sector 19, Gurugram, Haryana</h4>
+                </div>
+             </div>
+
+             </div>
+
+
+         </div>
+         
 
          <Footer />
      </div>
